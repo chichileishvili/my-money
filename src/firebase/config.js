2d7dd2659246d7e55b04da5deb827063b45b907e
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-09-22 15:14:55 +0400

    adding firebase/firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQEPIJO7Hv1GqrFJyvuBshSq2JO5u3hVI',
+
+  authDomain: 'mymoney-fbb5b.firebaseapp.com',
+
+  projectId: 'mymoney-fbb5b',
+
+  storageBucket: 'mymoney-fbb5b.appspot.com',
+
+  messagingSenderId: '420022326477',
+
+  appId: '1:420022326477:web:483be5c9eb2aff5c7b4584',
+}
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig)
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
