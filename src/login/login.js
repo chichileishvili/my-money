f185662ef58a5fb85d6e5e81a09327d49fa462a6
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-09-23 15:22:21 +0400

    add: login hook

--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,14 @@
 import styles from './login.module.css'
 import { useState } from 'react'
-
+import { useLogin } from '../hooks/useLogin'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { login, error, isPending } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
+    login(email, password)
   }
   return (
     <form onSubmit={handleSubmit} className={styles['login-form']}>
@@ -21,7 +22,13 @@ const Login = () => {
         <span>password:</span>
         <input type='password' onChange={(e) => setPassword(e.target.value)} value={password} />
       </label>
-      <button className='btn'> Login</button>
+      {!isPending && <button className='btn'> Login</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   )
 }
