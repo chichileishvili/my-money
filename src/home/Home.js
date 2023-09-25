1fb3f23bc171b79055b5c54344bb9073cec117d6
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-09-25 16:00:46 +0400

    add:modifing home page and adding collections in the firestore

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,16 @@
+import { useAuthContext } from '../hooks/useAuthContext'
 import styles from './home.module.css'
-
+import TransactionForm from './TransactionForm'
 const Home = () => {
-  return <div>Home</div>
+  const { user } = useAuthContext()
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid} />
+      </div>
+    </div>
+  )
 }
 
 export default Home
