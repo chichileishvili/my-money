e46ed9d34f1d066ae0183f4edde93b24f03b3b15
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-09-25 16:31:00 +0400

    add: listing transactions

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,17 @@
 import { useAuthContext } from '../hooks/useAuthContext'
 import styles from './home.module.css'
 import TransactionForm from './TransactionForm'
+import { useCollection } from '../hooks/useCollection'
+import TransactionList from './TransactionList'
 const Home = () => {
   const { user } = useAuthContext()
+  const { documents, error } = useCollection('transactions')
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
